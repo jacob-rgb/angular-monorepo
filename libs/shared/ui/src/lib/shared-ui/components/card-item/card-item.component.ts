7c13ce53ea56dcdd6e75b423dc4fa65fb945d477
DESKTOP-H976RTI\Administrador <jacoboramirezrivera6@gmail.com>
2024-03-08 17:31:00 +0100

    styles

--- libs/shared/ui/src/lib/shared-ui/components/card-item/card-item.component.ts
+++ libs/shared/ui/src/lib/shared-ui/components/card-item/card-item.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-card-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
